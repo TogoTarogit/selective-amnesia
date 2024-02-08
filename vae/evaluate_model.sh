184f275f9d2e566f24ac7ba0476b17228787a0ce
togoinmesprit <taro_togo@lmd.ist.hokudai.ac.jp>
2024-02-08 10:45:12 +0900

    データセットを変更できるように変更

--- vae/evaluate_model.sh
+++ vae/evaluate_model.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # GPUの指定を変数として甘止める
-cuda_num=0
+cuda_num=1
 # サンプルとして出力する画像の枚数
+# ----------------------------------------------------
+# 実験設定
 # n_samples=1000
 n_samples=10000
 
 # 結果保存用ディレクトリ
 result_save_dir="./results/text_results"
-
+contents_discription=""
+dataset="mnist" 
+# ----------------------------------
 # 結果保存用ディレクトリが存在しない場合は作成
 if [ ! -d "$result_save_dir" ]; then
     mkdir -p "$result_save_dir"
@@ -19,24 +23,52 @@ result_dir_name="$result_save_dir/$file_name"
 # 実験の日付を表示
 echo "experiment date: $(date "+%Y/%m/%d %H:%M:%S")" | tee -a $result_dir_name
 # 実験の内容について記録
-echo "experiment content: それぞれのモデルについて評価をする 評価方法はエントロピー すでに生成されてる場合スキップするように変更" | tee -a $result_dir_name
+echo "experiment content: $contents_discription" | tee -a $result_dir_name
 # ファイルに変数の値を追記
 echo "CUDA Number: $cuda_num" >> $result_dir_name
 echo "Number of Samples: $n_samples" >> $result_dir_name
-echo "------------------------------------------------" >> $result_dir_name
+echo "dataset is $dataset" >> $result_dir_name
 
 
-FILE_PATH="./2024_01_15_155632_mnist_forget_learn_test.txt" #ランダムに忘れる
-FILE_PATH="./2024_01_17_172647_mnist_forget_learn_test.txt" #　多分雑音画像
-FILE_PATH="./2024_01_09_mnist_forget_learn_test.txt" #テスト用
+# FILE_PATH="./" #テスト用
+# FILE_PATH="./results/text_results/2024_02_02_192716_mnist_forget_learn_test.txt" #mnist randam
+# FILE_PATH="./results/text_results/2024_02_02_192655_mnist_forget_learn_test.txt" #fashion random
+# FILE_PATH="./results/text_results/2024_02_01_121854_mnist_forget_learn_test.txt" #mnist noise
+# FILE_PATH="./results/text_results/2024_02_01_121854_fix_mnist_forget_learn_test_mnist_noise.txt" #mnist noise
+            # /results/text_results/2024_02_01_121854_fix_mnist_forget_learn_test_mnist_noise.txt
+# FILE_PATH="./fashion_noise_fix.txt" #
+# FILE_PATH="./fashion_random_fix.txt" #
+FILE_PATH="./mnist_random_fix.txt" #
+
 echo "FILE_PATH: $FILE_PATH" >> $result_dir_name
+# ファイルが存在するかチェック
+if [ -f "$FILE_PATH" ]; then
+    # ファイルの内容を出力
+    cat "$FILE_PATH"
+else
+    echo "ファイルが存在しません: $FILE_PATH"
+fi
+# cat $FILE_PATH >> $result_dir_name
+# mapfile -t directories < <(grep -oP './results/mnist/\d+_\d+_\d+_\d+/' "$FILE_PATH")
 
 # テキストファイルからディレクトリのパスを抽出して配列に格納
-mapfile -t directories < <(grep -oP './results/mnist/\d+_\d+_\d+_\d+/' "$FILE_PATH")
-
+# mapfile -t directories < <(grep -oP './results/mnist/(\d+_\d+_\d+_\d+)/' "$FILE_PATH")
+# mapfile -t directories < <(grep -oP './results/mnist/(\d+_)+/' "$FILE_PATH")
+# datasetに応じて適切な値を設定
+if [ "$dataset" = "mnist" ]; then
+    dataset_dir="mnist"
+elif [ "$dataset" = "fashion" ]; then
+    dataset_dir="fashionmnist"
+else
+    echo "不明なdataset: $dataset"
+    exit 1
+fi
+mapfile -t directories < <(grep -oP "./results/${dataset_dir}/(\d+_)+\d+" "$FILE_PATH")
 # デバッグ情報として取得したディレクトリを表示
+printf '%s\n' "${directories[@]}"
+echo "------------------------------------------------" >> $result_dir_name
+
 # echo "取得したディレクトリ:"
-# printf '%s\n' "${directories[@]}"
 
 # ディレクトリの数だけループ
 for directory in "${directories[@]}"
@@ -60,7 +92,9 @@ do
         
         # サンプル評価の出力をファイルに追記
         # echo "Evaluating samples for label $LABEL..." >> $result_dir_name
-        CUDA_VISIBLE_DEVICES=$cuda_num python evaluate_with_classifier.py --sample_path $directory --label_of_dropped_class $LABEL >> $result_dir_name
+        # CUDA_VISIBLE_DEVICES=$cuda_num python evaluate_with_classifier.py --sample_path $directory --label_of_dropped_class $LABEL >> $result_dir_name
+        CUDA_VISIBLE_DEVICES=$cuda_num python evaluate_with_classifier.py --sample_path $directory --label_of_dropped_class $LABEL --dataset $dataset >> $result_dir_name
+
     done
     echo "------------------------------------------------" >> $result_dir_name
 
